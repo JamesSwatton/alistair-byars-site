b20678f129f6c45c9021b9109df8774dae1bcca2
JamesSwatton <jamessswatton@gmail.com>
2025-03-24 21:39:57 +0000

    remove base plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,8 @@
 import getUrlPartial from "./src/_config/filters/get-url-part.js";
-import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
 export default function (config) {
   // Plugins
-  config.addPlugin(EleventyHtmlBasePlugin);
-  
+
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy({ "./src/images": "images" });
   config.addPassthroughCopy({ "./src/js/": "js" });
@@ -36,6 +34,7 @@ export default function (config) {
     dir: {
       input: "src/content",
       output: "dist",
+      pathPrefix: "/alistair-byars-site/",
       includes: "../_includes",
       data: "../_data",
     },
